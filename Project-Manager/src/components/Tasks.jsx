f22d40b327d6d0184d0fc1342330f9182009dfa3
roopesh-jsp <rupzkumar5@gmail.com>
2024-04-22 08:26:35 +0530

    tasks updated -- project manager

--- Project-Manager/src/components/Tasks.jsx
+++ Project-Manager/src/components/Tasks.jsx
@@ -1,21 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Tasks({ addTask }) {
   const [newTask, setNewTask] = useState();
   function handleChnage(event) {
     setNewTask(event.target.value);
+
+    // useEffect(() => {
+    // event.target.addEventListener("keypress", function (event) {
+    //   if (event.key === "Enter") {
+    //     addTask(newTask);
+    //     setNewTask("");
+    //   }
+    // });
+    // }, []);
   }
+  // let input = document.getElementById("input");
+
+  // input.addEventListener("keypress", function (event) {
+  //   if (event.key === "Enter") {
+  //     addTask(newTask);
+  //     setNewTask("");
+  //   }
+  // });
+
   return (
     <>
       <input
+        id="input"
         type="text"
         className="bg-stone-200 w-9/12 h-8 rounded hover:outline-none p-2"
         onChange={handleChnage}
-        // value={newTask}
+        value={newTask}
       />
       <button
         className="hover:bg-stone-200 p-2 rounded"
-        onClick={() => addTask(newTask)}
+        onClick={() => {
+          addTask(newTask);
+
+          setNewTask("");
+        }}
       >
         Add task
       </button>
