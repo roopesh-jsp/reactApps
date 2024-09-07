5089e3e7a8f5c7266f778ddbdf162f893823dc02
roopesh-jsp <rupzkumar5@gmail.com>
2024-09-07 16:56:31 +0530

    done

--- FoodApp/src/shop/CartContext.jsx
+++ FoodApp/src/shop/CartContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+ import { createContext, useReducer } from "react";
 
 export const CartContext = createContext({
   items: [],
--- routing-concepts/src/components/Home.jsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-export default function Home() {
-  return (
-    <>
-      <h1>home</h1>
-      <Link to="/products"> products</Link>
-    </>
-  );
-}
