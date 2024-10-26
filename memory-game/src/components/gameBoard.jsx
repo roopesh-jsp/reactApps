62a48680a7c1f6513721f82dd0253a19eda5dc6b
roopesh-jsp <rupzkumar5@gmail.com>
2024-10-26 11:25:44 +0530

    winning logic left

--- memory-game/src/components/gameBoard.jsx
+++ memory-game/src/components/gameBoard.jsx
@@ -1,14 +1,74 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function GameBoard() {
   const [NoGrids, setNoGrids] = useState(4);
+
+  const [cards, setCards] = useState([]);
+  const [flipped, setFlipped] = useState([]);
+  const [matched, setmatched] = useState([]);
+
+  const [disabled, setDisabled] = useState(false);
+  const [won, setWon] = useState(false);
+
   function onChangeGrids(e) {
     const size = parseInt(e.target.value);
     // console.log(size);
 
-    // if (size >= 2 && size <= 10)
-    setNoGrids(size);
+    if (size >= 2 && size <= 10) setNoGrids(size);
   }
+
+  function initGame() {
+    const totalCards = NoGrids * NoGrids;
+    const NoPairs = Math.floor(totalCards / 2);
+    const numbers = [...Array(NoPairs).keys()].map((ele, idx) => ele + 1);
+    const shuffledCards = [...numbers, ...numbers]
+      .sort((a, b) => Math.random() - 0.5)
+      .map((ele, idx) => ({ id: idx, number: ele }));
+    setCards(shuffledCards);
+  }
+
+  function isMatched(id) {
+    const [firstId] = flipped;
+    if (cards[firstId].number === cards[id].number) {
+      setmatched([...matched, firstId, id]);
+      setDisabled(false);
+      setFlipped([]);
+    } else {
+      setTimeout(() => {
+        setDisabled(false);
+        setFlipped([]);
+      }, 1000);
+    }
+  }
+
+  function onFlip(id) {
+    if (disabled || won) return;
+    if (flipped.length === 0) {
+      setFlipped([id]);
+      return;
+    }
+
+    if (flipped.length === 1) {
+      setDisabled(true);
+      if (id !== flipped[0]) {
+        setFlipped([...flipped, id]);
+        isMatched(id);
+      } else {
+        setDisabled(false);
+        setFlipped([]);
+      }
+    }
+  }
+
+  function isFlipped(id) {
+    return flipped.includes(id) || matched.includes(id);
+  }
+  function isSolved(id) {
+    return matched.includes(id);
+  }
+  useEffect(() => {
+    initGame();
+  }, [NoGrids]);
   return (
     <div className="w-fit ml-auto mr-auto mt-10 flex flex-col gap-10 justify-center">
       <h1 className="text-3xl text-center font-bold">Memory game</h1>
@@ -25,6 +85,32 @@ export default function GameBoard() {
           onChange={onChangeGrids}
           className="bg-slate-100 w-20 rounded-sm px-2 py-1 border-black border-2"
         />
+      </div>
+      {/* board */}
+      <div
+        className="grid gap-2 mb-4"
+        style={{
+          gridTemplateColumns: `repeat(${NoGrids},minmax(0,1fr))`,
+          width: `min(100%,${NoGrids * 6}em)`,
+        }}
+      >
+        {cards.map((ele, idx) => {
+          return (
+            <div
+              key={idx}
+              className={` flex  items-center justify-center cursor-pointer aspect-square font-bold rounded-lg ${
+                isFlipped(ele.id)
+                  ? isSolved(ele.id)
+                    ? "bg-green-500 text-white"
+                    : "bg-blue-500 text-white"
+                  : "bg-slate-300 text-black"
+              }`}
+              onClick={() => onFlip(ele.id)}
+            >
+              {isFlipped(ele.id) ? ele.number : "?"}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
